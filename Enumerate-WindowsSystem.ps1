a50571c1eef8da30f7259617ffa0851d317e668d
DrDoomScroll <mcmanis000@gmail.com>
2022-08-08 17:06:49 -0700

    Update Enumerate-WindowsSystem.ps1

--- Enumerate-WindowsSystem.ps1
+++ Enumerate-WindowsSystem.ps1
@@ -2,19 +2,19 @@
 systemInfo > .\"$env:ComputerName.SystemInfo.txt"
 
 # Gets service list
-Write-Progress -Activity "Getting Services" -Completed 0
+Write-Progress -Activity "Getting Services"
 Get-Service | Select DisplayName, ServiceName, Status | Sort DisplayName > .\"$env:ComputerName.Services.txt"
 
 # Gets process list
-Write-Progress -Activity "Getting Processes" -Completed 25
+Write-Progress -Activity "Getting Processes"
 Get-Process | Select Name, Id | Sort Name > .\"$env:ComputerName.Process.txt"
 
 # Gets installed applications. This is an older way but it has better compatibility with older systems.
-Write-Progress -Activity "Getting Installed Applications" -Completed 50
+Write-Progress -Activity "Getting Installed Applications"
 wmic Product get Name,Version,Vendor  > .\"$env:ComputerName.Applications.txt"
 
 # Gets a list of installed server features. Only works on Windows Server OS, not Windows Client OS.
-Write-Progress -Activity "Getting Installed Server Features and Roles" -Completed 75
+Write-Progress -Activity "Getting Installed Server Features and Roles"
 try{
     $windowsFeatures = Get-WindowsFeature
     if($windowsFeatures){
@@ -24,3 +24,6 @@ try{
 catch [System.Exception] {
     Write-Warning "Unable to get Windows Server Feature details because this is: $env:OS" 
 }
+
+# Gets NTP configurtion
+W32tm /query /computer:contosoW1 /configuration > .\"$env:ComputerName.w32tm.txt
