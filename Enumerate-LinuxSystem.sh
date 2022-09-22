2912d26fa5333029ebca99e512dc4d68ba2be6e4
DrDoomScroll <mcmanis000@gmail.com>
2022-09-22 16:44:54 -0700

    Update Enumerate-LinuxSystem.sh

--- Enumerate-LinuxSystem.sh
+++ Enumerate-LinuxSystem.sh
@@ -2,10 +2,19 @@
 HOST=$(uname -n)
 
 # Get OS information
-cat /proc/version >> ./$HOST.System.txt
+cat /proc/version > ./$HOST.System.txt
+
+# Get Env variables
+cat /etc/profile > ./$HOST.Env.txt
+cat /etc/bashrc >> ./$HOST.Env.txt
+cat ~/.bash_profile >> ./$HOST.Env.txt
+cat ~/.bashrc >> ./$HOST.Env.txt
 
 # Get service information
 systemctl list-units --type=service > ./$HOST.Services.txt
+
+# Get processes running as root
+ps aux | grep root > ./$HOST.RunningRootProcesses.txt
 
 # Get process list
 ps -aux > ./$HOST.Processes.txt
