f88abf799df830dea012014564f51f9543de830d
DrDoomScroll <mcmanis000@gmail.com>
2022-09-22 16:44:15 -0700

    Create Enumerate_LinuxSystem.py

---
+++ Enumerate_LinuxSystem.py
@@ -0,0 +1,28 @@
+import os
+
+os.system('''#!/bin/bash
+HOST=$(uname -n)
+
+# Get OS information
+cat /proc/version > ./$HOST.System.txt
+
+# Get Env variables
+cat /etc/profile > ./$HOST.Env.txt
+cat /etc/bashrc >> ./$HOST.Env.txt
+cat ~/.bash_profile >> ./$HOST.Env.txt
+cat ~/.bashrc >> ./$HOST.Env.txt
+
+# Get service information
+systemctl list-units --type=service > ./$HOST.Services.txt
+
+# Get processes running as root
+ps aux | grep root > ./$HOST.RunningRootProcesses.txt
+
+# Get process list
+ps -aux > ./$HOST.Processes.txt
+
+# Get listening ports
+netstat -l > ./$HOST.ListeningPorts.txt
+
+# Get installed applications
+dpkg-query -l > ./$HOST.InstalledApplications.txt''')
